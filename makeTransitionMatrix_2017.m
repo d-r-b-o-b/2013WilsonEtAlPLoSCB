8ef9c0ded4258dc3287b943b67fc2e84e678a315
bobUA <31321469+bobUA@users.noreply.github.com>
2017-09-12 15:37:48 -0700

    update to 2017 transition matrix

--- makeTransitionMatrix_2017.m
+++ makeTransitionMatrix_2017.m
@@ -4,9 +4,13 @@ function TrMat = makeTransitionMatrix_2017(l, h)
 TrMat(1,1:length(l)) = h;
 for i = 1:length(l)-1
     
-    if (i == 1) | ( abs(l(i)-l(i-1))<1 )
+    % EDIT if (i == 1) | ( abs(l(i)-l(i-1))<1 )
+    if (i == 1) 
         TrMat(1, i) = h + (1-h) * (l(i+1)-l(i)-1)/(l(i+1)-l(i));;
         TrMat(i+1, i) = (1-h) * 1/(l(i+1)-l(i));
+    elseif ( abs(l(i+1)-l(i))<1 )
+        TrMat(1, i) = h;
+        TrMat(i+1, i) = (1-h);
     else
         TrMat(1, i) = h;
         TrMat(i, i) = (1-h) * (l(i+1)-l(i)-1)/(l(i+1)-l(i));
